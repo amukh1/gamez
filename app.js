843d659eeb3ced08f06a40e3eb19dc17bcf3e7b5
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-11-28 15:40:01 -0500

    b

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function preload ()
     this.load.setBaseURL('http://labs.phaser.io');
 
     this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+    this.load.image('frog', 'frogger.png');
     this.load.image('red', 'assets/particles/red.png');
 }
 
